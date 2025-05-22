d1ff7194e745135482d1f1d8615424603aa69236
Antonio Erick <groot150x@gmail.com>
2025-05-22 14:19:19 -0300

    Exemplo de metodos genericos

--- Aulas/Aula21-05/src/Application/Program.java
+++ Aulas/Aula21-05/src/Application/Program.java
@@ -2,6 +2,7 @@ package Application;
 
 import Natal.Data;
 import Conversor.Unidades;
+import Igualdade.ehIgual;
 
 public class Program {
     public static void main(String[] args) {
@@ -24,5 +25,20 @@ public class Program {
         n2023.mostraData();
         n2024.mostraData();
         n2025.mostraData();
+
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("Demonstraçao de metodos genericos: ");
+
+        Double c1 = 5.0;
+        Double c2 = 5.0;
+
+        System.out.println("c1 == c2 ? "+ (ehIgual.Igual(c1, c2) ? "Sim" : "Nao"));
+
+        Integer d1 = 4;
+        Integer d2 = 5;
+
+        System.out.println("d1 == d2 ? "+ (ehIgual.Igual(d1, d2) ? "Sim" : "Nao"));
+
+        System.out.println("-----------------------------------------------------------------");
     }
 }
---
+++ Aulas/Aula21-05/src/Igualdade/ehIgual.java
@@ -0,0 +1,7 @@
+package Igualdade;
+
+public class ehIgual {
+    public static <T> boolean Igual(T a, T b){
+        return a.equals(b);
+    }
+}
